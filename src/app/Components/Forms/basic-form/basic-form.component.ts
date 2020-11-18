32cf775bcbc0c162c4db5bf3f9736eb6fb6db9f4
monis15 <monis.mazhar@iomechs.com>
2020-11-18 14:05:16 +0500

    practise some basic reactive form

--- src/app/Components/Forms/basic-form/basic-form.component.ts
+++ src/app/Components/Forms/basic-form/basic-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'basic-form',
@@ -6,30 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-form.component.css']
 })
 export class BasicFormComponent implements OnInit {
-  errorMessage:any = {
-    required: (field:string) => {
-      return `${field} is required!`
-    },
-    minLength: (field:string, limit:number) => {
-      return `${field} should be minimum ${limit} characters long!`
-    },
-    maxLength: (field:string, limit:number) => {
-      return `${field} should not be more than ${limit} characters long!`
-    }
-  }
+
+  address: FormGroup = new FormGroup({
+    state: new FormControl(''),
+    city: new FormControl(''),
+    street: new FormControl(''),
+  })
+
+  userProfile:FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    password: new FormControl(''),
+    type: new FormControl(''),
+    address: this.address, 
+    hobbies: new FormArray([
+      new FormControl(''),
+    ])
+  })
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //Getter for the array control of the form
+  get hobbies(){
+    return this.userProfile.get('hobbies') as FormArray;
+  }
 
-  handleSubmitForm(f:any){
-    if(f.invalid){
-      alert("Invalid Data enetered!");
-      return;
-    }
-    console.log("values are ", f.value);
+  //Adding new hobbies
+  addHobby(){
+    this.hobbies.push(new FormControl(''));
+  }
+
+
+  handleSubmitForm(){
+    console.log(this.userProfile.value);
+    
+  }
+
+  updateForm(){
+    this.userProfile.patchValue({
+      firstName: 'Monis',
+      email: 'monisms15@gmail.com',
+      address: {
+        state: 'USA',
+        city: 'California',
+        street: '123',
+      }
+    })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicFormComponent } from './Components/Forms/basic-form/basic-form.component';
 import { AppHeaderComponent } from './Layout/app-header/app-header.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AppHeaderComponent } from './Layout/app-header/app-header.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
